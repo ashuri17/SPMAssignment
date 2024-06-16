6c45354ec500dca65e1e159fddc52017ca7e7ab4
Ryan <s10261024@connect.np.edu.sg>
2024-06-16 17:08:11 +0800

    edit

--- NgeeAnnCity/NgeeAnnCity/SaveLoad.cs
+++ NgeeAnnCity/NgeeAnnCity/SaveLoad.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Text.Json;
 
 public class SaveFile
@@ -8,4 +9,57 @@ public class SaveFile
     public string Location {get; set;}
     public string Stats {get; set;}
     public string FilePath {get; set;}
+
+    private static SaveFile? LoadFile()
+    {
+        string saveFile;
+
+        // directory path for save files
+        string saveFilesDirectory = "";
+
+        // get save files in directory
+        string[] saveFiles = Directory.GetFiles(saveFilesDirectory);
+
+        // check if directory is empty
+        if (!saveFiles.Any())
+        {
+            return null;
+        }
+
+        // print save files for user to choose
+        for (int i = 0; i < saveFiles.Length; i++)
+        {
+            Console.WriteLine($"[{i}] {saveFiles[i]}");
+        }
+
+
+        while (true)
+        {
+            Console.WriteLine("\nEnter the number corresponding to the saved game: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int result))
+            {
+                Console.WriteLine("Invalid option");
+                continue;
+            }
+
+            if (result < 0 || result > saveFiles.Length - 1)
+            {
+                Console.WriteLine("That savefile does not exist.");
+                continue;
+            }
+
+            saveFile = saveFiles[result];
+            break;
+        }
+
+        // Jian Hui's code
+        using (StreamReader r = new StreamReader(saveFile)) //Reading file
+        {
+            string jsonString = r.ReadToEnd();
+            return JsonSerializer.Deserialize<SaveFile>(jsonString);
+        }
+
+
+    }
 }
