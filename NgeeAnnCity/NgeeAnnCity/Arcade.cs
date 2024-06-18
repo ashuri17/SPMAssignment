4838ba06173596ba6a8abe1b2e159e8e996cc678
Ryan <s10261024@connect.np.edu.sg>
2024-06-18 15:26:05 +0800

    add grid labels

--- NgeeAnnCity/NgeeAnnCity/Arcade.cs
+++ NgeeAnnCity/NgeeAnnCity/Arcade.cs
@@ -55,7 +55,7 @@ namespace NgeeAnnCity
 
                 // Select two random buildings
                 List<string> selectedBuildings = SelectBuildings();
-                DisplaySelectedBuildings(selectedBuildings);
+                
 
                 // Get player choice
                 string chosenBuilding = GetPlayerChoice(selectedBuildings);
@@ -168,33 +168,42 @@ namespace NgeeAnnCity
 
         private void DisplayBoard()
         {
-            // get width of board 
+            int horizontalPadding = this.width.ToString().Length + 1;
 
-            // get height of board
-            for (int i = 0; i < 20; i++)
+            // get grid labels
+            char[][] gridLabels = GetGridLabels();
+
+            // print top grid labels
+            for (int i = 0; i < gridLabels.Length; i++)
             {
-                for (int j = 0; j < 20; j++)
+                Console.Write(new String(' ', horizontalPadding + 2));
+                Console.WriteLine(string.Join(" ", gridLabels[i]));
+            }
+
+            Console.WriteLine();
+
+            // print grid
+            for (int i = 0; i < this.height; i++)
+            {
+                // grid label on the left
+                Console.Write($"{i + 1}".PadLeft(horizontalPadding) + "  "); 
+
+                for (int j = 0; j < width; j++)
                 {
                     Console.Write(board[i, j] + " ");
                 }
+
                 Console.WriteLine();
             }
+            Console.WriteLine("\n\n");
         }
 
         private void DisplayStats()
         {
+            Console.WriteLine(new string('-', 10) + "ARCADE MODE" + new string('-', 10) + "\n");
             Console.WriteLine($"Turn: {turnNumber}");
             Console.WriteLine($"Coins left: {coins}");
             Console.WriteLine($"Points: {points}");
-        }
-
-        private void DisplaySelectedBuildings(List<string> selectedBuildings)
-        {
-            Console.WriteLine("Selected buildings for this turn:");
-            foreach (string building in selectedBuildings)
-            {
-                Console.WriteLine(building);
-            }
         }
 
         private void CalculatePointsAndCoins()
@@ -208,6 +217,71 @@ namespace NgeeAnnCity
         {
             // Placeholder for actual upkeep cost calculation
             coins -= 10; // Example decrement
+        }
+
+        private char[][] GetGridLabels()
+        {
+            // Get each number to print
+            int[] numbers = Enumerable.Range(1, this.width).ToArray();
+
+            // Convert each number to a string
+            string[] numberStrings = numbers.Select(i => i.ToString()).ToArray();
+
+            // Max number of digits for each number => number of digits the largest number has => number of digits the width has
+            int maxLength = this.width.ToString().Length;
+
+            // Number of rows the array should have
+            char[][] rows = new char[maxLength][];
+
+            // Number of columns (digits) each row should have
+            for (int i = 0; i < maxLength; i++)
+            {
+                rows[i] = new char[this.width];
+            }
+
+            // Iterate through each number
+            for (int col = 0; col < width; col ++)
+            {
+                string num = numberStrings[col];
+                int numLen = num.Length;
+
+                // Iterate through each digit
+                for (int row = 0; row < maxLength; row++)
+                {
+                    // check if num has a digit in the row (99 doesn't have a digit in the first row if the width is 100) 
+                    // e.g.
+                    //
+                    //        1
+                    //   9    0
+                    //   9    0
+           
+                    if (row < numLen)
+                    {
+                        // If width is 2 digits, there will be 2 rows.
+                        // e.g.
+                        // {...} <- stores the digit in the "tens" place
+                        // {...} <- stores the digit in the "ones" place
+                        //
+                        // If num is 5, numLen is 1.
+                        // During the first iteration of the for loop, the if statement will evaluate to (0 < 1).
+                        // Since it is true, 5 will be added to the last row.
+                        //
+                        // If it is 25, 5 will be added to the last row.
+                        // If it is 125, 5 will be added to the last row.
+                        //
+                        //
+                        // Subsequent iterations will check the other digits and if possible, add them to the other rows.
+
+                        rows[maxLength - row - 1][col] = num[numLen - row - 1];
+                    }
+                    else
+                    {
+                        // If there is no digits, represent as whitespace
+                        rows[maxLength - row - 1][col] = ' ';
+                    }
+                }
+            }
+            return rows;
         }
     }
 }
