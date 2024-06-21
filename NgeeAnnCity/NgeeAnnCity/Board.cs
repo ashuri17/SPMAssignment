6169c24d26787541e41e3c917350a0f233d1d756
Ryan <s10261024@connect.np.edu.sg>
2024-06-21 19:24:10 +0800

    created board class

---
+++ NgeeAnnCity/NgeeAnnCity/Board.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NgeeAnnCity
+{
+    internal class Board
+    {
+        private int size;
+        private char[,] grid;
+
+        public Board(int size)
+        {
+            this.size = size;
+            this.grid = new char[size, size];
+        }
+
+        internal void Initialize()
+        {
+            for (int i = 0; i < this.size; i++)
+            {
+                for (int j = 0; j < this.size; j++)
+                {
+                    this.grid[i, j] = '.';
+                }
+            }
+        }
+
+        internal void Display()
+        {
+            int horizontalPadding = this.size.ToString().Length + 1;
+
+            // get grid labels
+            char[][] gridLabels = GetGridLabels();
+
+            // print top grid labels
+            for (int i = 0; i < gridLabels.Length; i++)
+            {
+                Console.Write(new String(' ', horizontalPadding + 2));
+                Console.WriteLine(string.Join(" ", gridLabels[i]));
+            }
+
+            Console.WriteLine();
+
+            // print grid
+            for (int i = 0; i < this.size; i++)
+            {
+                // grid label on the left
+                Console.Write($"{i + 1}".PadLeft(horizontalPadding) + "  ");
+
+                for (int j = 0; j < this.size; j++)
+                {
+                    Console.Write(this.grid[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        private char[][] GetGridLabels()
+        {
+            // Get each number to print
+            int[] numbers = Enumerable.Range(1, this.size).ToArray();
+
+            // Convert each number to a string
+            string[] numberStrings = numbers.Select(i => i.ToString()).ToArray();
+
+            // Max number of digits for each number => number of digits the largest number has => number of digits the width has
+            int maxLength = this.size.ToString().Length;
+
+            // Number of rows the array should have
+            char[][] rows = new char[maxLength][];
+
+            // Number of columns (digits) each row should have
+            for (int i = 0; i < maxLength; i++)
+            {
+                rows[i] = new char[this.size];
+            }
+
+            // Iterate through each number
+            for (int col = 0; col < this.size; col++)
+            {
+                string num = numberStrings[col];
+                int numLen = num.Length;
+
+                // Iterate through each digit
+                for (int row = 0; row < maxLength; row++)
+                {
+                    // check if num has a digit in the row (99 doesn't have a digit in the first row if the width is 100) 
+                    // e.g.
+                    //
+                    //        1
+                    //   9    0
+                    //   9    0
+
+                    if (row < numLen)
+                    {
+                        // If width is 2 digits, there will be 2 rows.
+                        // e.g.
+                        // {...} <- stores the digit in the "tens" place
+                        // {...} <- stores the digit in the "ones" place
+                        //
+                        // If num is 5, numLen is 1.
+                        // During the first iteration of the for loop, the if statement will evaluate to (0 < 1).
+                        // Since it is true, 5 will be added to the last row.
+                        //
+                        // If it is 25, 5 will be added to the last row.
+                        // If it is 125, 5 will be added to the last row.
+                        //
+                        //
+                        // Subsequent iterations will check the other digits and if possible, add them to the other rows.
+
+                        rows[maxLength - row - 1][col] = num[numLen - row - 1];
+                    }
+                    else
+                    {
+                        // If there is no digits, represent as whitespace
+                        rows[maxLength - row - 1][col] = ' ';
+                    }
+                }
+            }
+            return rows;
+        }
+
+        internal void PlaceBuilding(char building)
+        {
+            int x, y;
+
+            // runs until a building is placed
+            while (true)
+            {
+                // get row from user
+                while (true)
+                {
+                    Console.Write($"Row (1-{this.size}): ");
+
+                    // check if user enters a number that falls within the width of the board
+                    if (!int.TryParse(Console.ReadLine(), out x) || x < 1 || x > this.size)
+                    {
+                        Console.WriteLine("Invalid row.\n");
+                        continue;
+                    }
+                    break;
+                }
+
+                // get column from user 
+                while (true)
+                {
+                    Console.Write($"Column (1-{this.size}): ");
+
+                    // check if user enters a number that falls within the height of the board
+                    if (!int.TryParse(Console.ReadLine(), out y) || y < 1 || y > this.size)
+                    {
+                        Console.WriteLine("Invalid column.\n");
+                        continue;
+                    }
+                    break;
+                }
+
+                // check if spot is taken
+                if (this.grid[x-1, y-1] != '.')
+                {
+                    Console.WriteLine("Spot taken.\n");
+                }
+                else
+                {
+                    this.grid[x-1, y-1] = building;
+                    break;
+                }
+            }
+        }
+    }
+}
